4590d56b7c87c19272c18c1e4052444f5534d074
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-03-05 16:34:30 -0700

    Added a test icon

---
+++ README.md
@@ -0,0 +1,42 @@
+# Fantasy Map with Info Markers
+
+This is a map of a region in my fantasy D&D setting. It has markers for cities and 
+important locations. It also contains basic information about those locations.
+
+## Instructions for Build and Use
+
+Steps to build and/or run the software:
+
+1. First step here
+2.
+3.
+
+Instructions for using the software:
+
+1. First step here
+2.
+3.
+
+## Development Environment
+
+To recreate the development environment, you need the following software and/or libraries with the specified versions:
+
+* First thing here
+*
+*
+
+## Useful Websites to Learn More
+
+I found these websites useful in developing this software:
+
+* [Website Title](Link)
+*
+*
+
+## Future Work
+
+The following items I plan to fix, improve, and/or add to this project in the future:
+
+* [ ] First thing here
+* [ ]
+* [ ]
--- main.js
+++ main.js
@@ -3,12 +3,27 @@ var map = L.map('map', {
 });
 
 // Define image size in pixels
-var bounds = [[0,0], [1000,1000]]; // height, width in pixels
-
+var bounds = [[0,0], [1024,1327]]; // height, width in pixels 3981/3072
 L.imageOverlay('images/primior-map.jpg', bounds).addTo(map);
-
 map.fitBounds(bounds);
+// map.setMaxBounds(bounds);
 
+var skullIcon = L.icon({
+    iconUrl: 'images/witherskull.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [50, 50], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [25, 25], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+
+var marker = L.marker([200, 300], {icon: skullIcon}).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+
+var popup = L.popup();
 // var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // var circle = L.circle([51.508, -0.11], {
@@ -23,18 +38,5 @@ map.fitBounds(bounds);
 //     [51.503, -0.06],
 //     [51.51, -0.047]
 // ]).addTo(map);
-var marker = L.marker([200, 300]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
 
-var popup = L.popup();
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
